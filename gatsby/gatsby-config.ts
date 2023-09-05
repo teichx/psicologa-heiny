fb3b6055d91d960fc55a5ad9dee157fe69557f0d
teichx <teichx@hotmail.com>
2023-09-05 20:20:09 -0300

    build: Disable graphql

--- gatsby/gatsby-config.ts
+++ gatsby/gatsby-config.ts
@@ -5,10 +5,7 @@ const config: GatsbyConfig = {
     title: 'psicologa-heiny',
     siteUrl: 'https://www.yourdomain.tld',
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
+  graphqlTypegen: false,
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-image',
